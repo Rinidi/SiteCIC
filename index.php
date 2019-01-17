dfcb001909456c795d99cccab7af87b6fe50003d
Renan Vitor <renanpiranguinho@gmail.com>
2019-01-16 22:20:03 -0200

    Iniciando Pagina Index pronta

---
+++ index.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/estilo.css">
+    </head>
+    <body>
+        <div>
+            <nav id="navSecundaria" class="navbar fixed-top navbar-expand-lg navbar-dark ">                
+                <div id="contNav" class="container justify-content-center">                                   
+                    <a class="navbar-brand" href="#"><img src="img/Escola-Principal.png" height="80px"></a>
+                    <a class="navbar-brand" href="#"><img src="img/positivo1.png" height="80px"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarText">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Sobre Nós
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                  <a class="dropdown-item" href="#">A escola</a>
+                                  <a class="dropdown-item" href="#">Matrícula</a>
+                                  <!-- <a class="dropdown-item" href="#">Regimento</a> -->
+                                  <a class="dropdown-item" href="#">Infraestrutura</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Educação
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                  <a class="dropdown-item" href="#">Educação Infantil</a>
+                                  <a class="dropdown-item" href="#">Fundamental I</a>
+                                  <a class="dropdown-item" href="#">Fundamental II</a>
+                                  <a class="dropdown-item" href="#">Ensino Médio</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Trabalhe conosco</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Fale conosco</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div> 
+            <nav class="navbar fixed-top navbar-expand-lg" id="navPrimaria"> 
+                <div class="container justify-content-end">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <img src="img/open-iconic-master/svg/phone.svg" alt="logo Telefone" height="15px">
+                            <span class="">(35)3641-1577</span>
+                        </li>
+                        <li class="nav-item">
+                            <a class="" href="#">
+                                <img src="img/open-iconic-master/svg/people.svg" alt="logo pessoa" height="15px">
+                                Pais e Alunos
+                            </a>
+                        </li>
+                    </ul>
+                </div>                  
+            </nav>
+        </div>
+        <div id="car">
+            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="img/slide1.svg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="img/slide2.svg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="img/slide3.svg" alt="Third slide">
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+        </div>
+        <div id="divicons" class="container">
+            <div class="row">
+                <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                    <a href="#"><img src="img/01.png" height="70px"></a>
+                </div>
+                <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                    <a href="#"><img src="img/02.png" height="70px"></a>
+                </div>
+                <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                    <a href="#"><img src="img/03.png" height="70px"></a>
+                </div>
+                <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                    <a href="#"><img src="img/04.png" height="70px"></a>
+                </div>
+            </div>
+        </div>
+            <article>
+                <div id="forms"class="container">
+                    <div class="row">
+                        <div class="col-2">
+                        </div>
+                        <div class="col-5">
+                            <h1>TEXTO TEXTO</h1>
+                            <h2>TEXTO TEXTO TEXTO</h2>
+                            <p>TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO TEXTO</p>
+                        </div>
+                        <div class="col-5">
+
+                        </div>  
+                    </div>
+                </div>
+            </article>
+        <footer>
+            <div id="dataFooter"class="container">
+                <div class="row">
+                    <div class="col-3">
+                        <h6>COLÉGIO IMACULADA CONCEIÇÃO</h6>
+                        <p>Texto Contando brevemente a história do Colégio Imaculada Conceição, o sem tempo de fundação juntamente com sua localidade, seus princípios educativos e objetivos como instituição de ensino[...],escrevendo sua história de forma profunda e transformadora...</p>
+                    </div>
+                    <div class="col-3">
+                        <h6>Níveis de Ensino</h6>
+                        <button type="button" class="btn btn-link">Educação Infantil</button>
+                        <button type="button" class="btn btn-link">Fundamental I</button>
+                        <button type="button" class="btn btn-link">Fundamental II</button>
+                        <button type="button" class="btn btn-link">Ensino Médio</button>
+                    </div>
+                    <div class="col-3">
+                        <h6>Fale Conosco</h6>
+                        <p>Rua Capitão Almeida Vergueiro 37530-000 Brazópolis - MG</p><br/>
+                        <p>Telefone:<br/> (35) 3641-1577</p>
+                    </div>
+                    <div class="col-3">
+                        <h6>Redes Sociais</h6>
+                        <a href="https://www.facebook.com/ciccolegioimaculada">
+                            <span>@ciccolefioimaculada</span>
+                            <img class="rounded" src="img/fb.png" alt="Logo Facebook" height="35px" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="barra"></div>
+            <div class="container">
+                Colégio Imaculada Conceição - 2019 
+            </div>
+        </footer>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="jquery/dist/jquery.js"></script>
+        <script src="popper.js/dist/popper.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+
+    </body>
+</html>
